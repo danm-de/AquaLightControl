fdb05005266eae9fe1697477370f75bf015dd233
Daniel Mueller <daniel@danm.de>
2014-06-10 22:35:52 +0200

    Added AquaLightControl/AquaLightControlService

---
+++ src/AquaLightControl/AquaLightControlService/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceProcess;
+
+namespace AquaLightControlService
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        static void Main() {
+            var services_to_run = new ServiceBase[]{ 
+                new Service() 
+            };
+
+            ServiceBase.Run(services_to_run);
+        }
+    }
+}
---
+++ src/AquaLightControl/AquaLightControlService/Service.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceProcess;
+
+namespace AquaLightControlService
+{
+    public partial class Service : ServiceBase
+    {
+        public Service() {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args) {
+        }
+
+        protected override void OnStop() {
+        }
+    }
+}
